3f2a2def94efd6c27183f36febddacb09f13bb60
AC <39713625+R0MULLUS@users.noreply.github.com>
2023-05-05 19:05:50 -0400

    login state

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,9 +1,21 @@
-export default function LoginPage(){
+import { useContext, useState } from "react";
+import { UserContext } from "../UserContext";
+
+export default function LoginPage() {
+    const [username, setUsername] = useContext(UserContext)
+    const [usernameState, setUsernameState] = useState('')
+    const [password, setPassword] = useState('')
+    function handleLogin(ev) {
+        ev.preventDefault()
+        setUsername()
+    }
     return (
-        <form className="login">
-            <input type="text" placeholder="username"/>
-            <input type="password" placeholder="password"/>
-            <button>Login</button>
+        <form onSubmit={handleLogin} className="login">
+            <input value={usernameState} type="text" placeholder="username" onChange={ev => { setUsernameState(ev.target.value) }} />
+            <input value={password} onChange={ev => { setPassword(ev.target.value) }} type="password" placeholder="password" />
+            <button> Login</button>
+            {usernameState}
+            {password}
         </form>
     );
 }
