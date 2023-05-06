88a05d5e482088cbab4aac443f215e58b0763f7c
StoneDagher <115055489+Andrew-Dagher@users.noreply.github.com>
2023-05-06 19:57:49 -0400

    add button

--- client/src/AddMemberForm.jsx
+++ client/src/AddMemberForm.jsx
@@ -96,7 +96,11 @@ export default function AddMemberForm({ setParent }) {
                     </div>
                   )))}
                   <div>
-                    <button value={member.member} type="submit" className="bg-[#333] text-white w-10 items-center align-items hover:bg-red-500">Add</button>
+                    <button type="submit" className='button manageSubmit' value='Add' ><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                    </button>
+
                   </div>
                 </div>
               </form>
