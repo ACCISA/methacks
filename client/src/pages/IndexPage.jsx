2caff8212243533f1e6ec9ef7278d63a4490ae3e
StoneDagher <115055489+Andrew-Dagher@users.noreply.github.com>
2023-05-06 11:47:01 -0400

    added form in index

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -3,11 +3,14 @@ import React, { useState, useEffect } from 'react';
 import Header from '../Header';
 import HeaderIndex from "../HeaderIndex";
 import { FaCheckCircle } from "react-icons/fa";
+import axios from "axios"
 
 export default function IndexPage() {
   const [url, setUrl] = useState('');
   const [file, setFile] = useState(null);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [documents, setDocuments] = useState([]);
+
 
   const bg = {
     backgroundImage: 'url("src/images/background.jpg")',
@@ -27,6 +30,14 @@ export default function IndexPage() {
     top: '0'
   }
 
+
+  useEffect(() => {
+    axios.get("data").then(response => {
+      setDocuments(response.data);
+    });
+  }, []);
+
+  
   useEffect(() => {
     const storedLoginStatus = localStorage.getItem("isLoggedIn");
     setIsLoggedIn(storedLoginStatus === "true");
@@ -58,7 +69,25 @@ export default function IndexPage() {
       </div>
       <div className="Buttons mt-25 pd-[20px] flex justify-center space-x-2 sm:space-x-4 md:space-x-6 lg:space-x-8 xl:space-x-12">
         <button id="uploadBtn" className="bg-green-600 hover:bg-green-700 active:bg-green-800 rounded-md w-[125px] py-3 px-2 mt-10" >Find Recipe</button>
+     
       </div>
+      <div className="flex justify-center pt-10">
+      <h1 className="">List of Documents:</h1>
+      <form className="flex flex-col ml-2">
+  {documents &&
+    documents.map(data => (
+      <div key={data.name} className="flex items-center mb-1">
+        <input className="mt-1.5" type="radio" id={data.name} name="groupName" value={data.name} />
+        <label htmlFor={data.name} className="ml-1">{data.name}</label>
+      </div>
+    ))}
+</form>
+
+
+
+
+    </div>
+    
     </div>
   )
 }
