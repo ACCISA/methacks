52402d8e6b67de553eac952bcca758a2cd602ecd
StoneDagher <115055489+Andrew-Dagher@users.noreply.github.com>
2023-05-06 00:10:46 -0400

    manage drop down form

--- client/src/AddMemberForm.jsx
+++ client/src/AddMemberForm.jsx
@@ -1,23 +1,28 @@
 import React, { useState } from 'react';
+import { FaChevronUp, FaChevronDown } from 'react-icons/fa';
 
 function AddMemberForm() {
-    const [showForm, setShowForm] = useState(false);
+  const [showForm, setShowForm] = useState(false);
 
-    return (
-        <div>
-            <button onClick={() => setShowForm(showForm ? false : true)}>+ Add Member</button>
-            {showForm && (
-                <form className="show">
-                    {<div>
-                        <input type='text' placeholder='username'></input>
-                        <textarea name="dietRestrictions" placeholder="Enter your dietary restrictions"></textarea>
-                    </div>
-
-                    }
-                </form>
-            )}
-        </div>
-    );
+  return (
+    <div>
+      <button className='manageButton' onClick={() => setShowForm(!showForm)}>
+      Add Member {showForm ? <FaChevronDown /> : <FaChevronUp />}
+        &nbsp;
+      </button>
+      {showForm && (
+        <form className="show borderForm">
+          <div>
+            <label htmlFor="username">Please enter the username of the new member:</label>
+            <input type='text' placeholder='username' className='w-1/3 mb-4' />
+            <label htmlFor="dietRestrictions" className='my-16'>Please enter all your dietary restrictions separated by a comma:</label>
+            <input type='text' name="dietRestrictions" placeholder="Enter your dietary restrictions" className='w-full border'></input>
+            <input type="submit" className='button manageSubmit' placeholder='Add'value='Add'/>
+          </div>
+        </form>
+      )}
+    </div>
+  );
 }
 
 export default AddMemberForm;
