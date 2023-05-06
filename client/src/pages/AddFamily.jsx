a9031b1541d33ebcae6ad2e2b42d940e58863ac1
Zubeda Wajid Hameed <114757482+ZubedaWajid@users.noreply.github.com>
2023-05-06 14:27:46 -0400

    fam page

--- client/src/pages/AddFamily.jsx
+++ client/src/pages/AddFamily.jsx
@@ -1,9 +1,23 @@
 import AddFamilyForm from "../AddFamilyForm";
+const bgFam = {
+    backgroundImage: 'url("src/images/webImg.webp")',
 
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    color: "white",
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    width: "100%",
+    height: '100%'
+}
 export default function AddFamily() {
     return (
         <>
+        <div style={bgFam} id="bkng2" className="p-0 h-full  flex-col justify-center align-middle">
             <AddFamilyForm />
+        </div>
         </>
     )
 }
