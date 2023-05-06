6b3af280c9ec35ad9b6800f7ad93fe09e730dee2
Zubeda Wajid Hameed <114757482+ZubedaWajid@users.noreply.github.com>
2023-05-06 03:05:22 -0400

    bug fix

--- client/src/HeaderIndex.jsx
+++ client/src/HeaderIndex.jsx
@@ -1,15 +1,41 @@
 import { Link } from 'react-router-dom';
 import './App.css';
+import { useContext } from 'react';
+import { UserContext } from './UserContext';
 
 export default function HeaderIndex(){
+    const {username, setUsername} = useContext(UserContext)
+    function handleLogout() {
+      setUsername(null);
+    }
+    
     return (
         <header className='mx-96 mt-8'>
-        <Link to="/" className="logo">MyBlog</Link>
+        <Link to="/" className="logo">
+        MyBlog
+      </Link>
 
-        <nav>
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
-        </nav>
+      <nav>
+        {username && (
+          <div>
+            <Link to="/manage" className="mr-2">
+              Manage
+            </Link>
+            <Link onClick={handleLogout} to="/">
+              Logout
+            </Link>
+          </div>
+        )}
+
+        {!username && (
+          <div>
+            <Link to="/login" className="mr-2">
+              Login
+            </Link>
+            <Link to="/register">Register</Link>
+          </div>
+        )}
+      </nav>
       </header>
     );
 }
