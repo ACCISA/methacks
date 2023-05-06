223b7a32cfa36bd076ad3259c4eca08d231cb7ca
AC <39713625+R0MULLUS@users.noreply.github.com>
2023-05-05 21:58:20 -0400

    merge issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,8 @@ import IndexPage from "./pages/IndexPage"
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import { UserContextProvider } from './UserContext';
-<<<<<<< Updated upstream
 import axios from "axios"
-=======
 import ManagePage from './pages/ManagePage';
->>>>>>> Stashed changes
 
 axios.defaults.baseURL = "http://localhost:4000";
 axios.defaults.withCredentials = true;
@@ -24,7 +21,7 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/manage" element={<ManagePage />} />
-        
+
         </Route>
       </Routes>
     </UserContextProvider>
