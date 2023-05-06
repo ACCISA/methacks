a86bd81cfae70ba8c15cf7fe702576a32412f0e6
Zubeda Wajid Hameed <114757482+ZubedaWajid@users.noreply.github.com>
2023-05-06 02:36:05 -0400

    as
    q

--- client/src/FormURL.jsx
+++ client/src/FormURL.jsx
@@ -35,13 +35,13 @@ export default function FormURL() {
         <div style={bg} id= "bkng" className="p-0 h-full  flex-col justify-center align-middle">
             <Header/>
            
-            <div class=" searchBar content-center mt-[48px] flex items-center justify-center">
+            <div className=" searchBar content-center mt-[48px] flex items-center justify-center">
                 <form onSubmit={handleUrlForm} className="w-96 mt-32 w-100 text-center">
                     <input value={url} onChange={ev => {setUrl(ev.target.value)}} type="text" className='text-black' placeholder="Paste URL" />
                     <input  className="bg-gray-200" type="file" />
                 </form>
             </div>
-            <div class="Buttons mt-25 pd-[20px] flex justify-center space-x-2 sm:space-x-4 md:space-x-6 lg:space-x-8 xl:space-x-12">
+            <div className="Buttons mt-25 pd-[20px] flex justify-center space-x-2 sm:space-x-4 md:space-x-6 lg:space-x-8 xl:space-x-12">
                 <button id="folderBtn" className=" bg-blue-600 rounded-full w-28 mt-10">Upload Image</button>
                 <button id="uploadBtn" className=" bg-blue-600 rounded-full w-28 mt-10" >Find Recipe</button>
            
