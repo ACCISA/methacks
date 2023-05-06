d4cf94231d3b61f875871cead88c4805b1901a53
AC <39713625+R0MULLUS@users.noreply.github.com>
2023-05-06 00:09:39 -0400

    Family set and get

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const User = require("./models/User.js");
+const Family = require("./models/Family.js");
 
 const app = express();
 const bcrypt = require("bcryptjs");
@@ -25,6 +26,47 @@ app.get("/test", (req, res) => {
   res.json("test ok");
 });
 
+app.post("/create", async (req, res) => {
+  const { token } = req.cookies;
+  const { name, restrictions, description, members } = req.body;
+  jwt.verify(token, jwtSecret, {}, async (err, user) => {
+    if (err) throw err;
+    try {
+      const famDoc = await Family.create({
+        owner: user.id,
+        name: name,
+        restrictions: restrictions,
+        description: description,
+        members: members,
+      });
+      res.json(famDoc);
+    } catch (errs) {
+      console.log("asdds");
+      console.log(errs);
+    }
+  });
+});
+
+app.get("/data", (req, res) => {
+  const { token } = req.cookies;
+  if (token) {
+    jwt.verify(token, jwtSecret, {}, async (err, user) => {
+      if (err) throw err;
+      try {
+        const fam = await Family.findOne({
+          owner: user.id,
+        });
+        res.json(fam);
+      } catch (errs) {
+        res.json("no family found");
+      }
+    });
+  } else {
+    console.log("sasd");
+    res.json(null);
+  }
+});
+
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
   try {
@@ -39,7 +81,7 @@ app.post("/register", async (req, res) => {
 });
 
 app.post("/login", async (req, res) => {
-  const {username,password } = req.body;
+  const { username, password } = req.body;
   const userDoc = await User.findOne({ username });
   if (userDoc) {
     const passOk = bcrypt.compareSync(password, userDoc.password);
--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -2,9 +2,8 @@ import { Link } from 'react-router-dom';
 import './App.css';
 import { useContext } from 'react';
 import { UserContext } from './UserContext';
+import axios from "axios"
 
-
- 
 
 export default function Header() {
   const { username, setUsername } = useContext(UserContext);
@@ -12,7 +11,15 @@ export default function Header() {
   function handleLogout() {
     setUsername(null);
   }
-
+  function handleTest() {
+    axios.get("/data", { username })
+    // axios.post("/create", {
+    //   name: "testing",
+    //   restrictions: ["res1", "res2", "res3"],
+    //   description: "this is a test",
+    //   members: { "friend1": "halal", "friend2": "diabetes" }
+    // })
+  }
   return (
     <header>
       <Link to="/" className="logo">
@@ -20,6 +27,7 @@ export default function Header() {
       </Link>
 
       <nav>
+        <Link onClick={handleTest}>Test</Link>
         {username && (
           <div>
             <Link to="/manage" className="mr-2">
