443ba61bf093b195d5a5f682219260d68babed4f
Zubeda Wajid Hameed <114757482+ZubedaWajid@users.noreply.github.com>
2023-05-06 21:08:09 -0400

    refresh issue fix

--- client/src/AddFamilyForm.jsx
+++ client/src/AddFamilyForm.jsx
@@ -53,8 +53,8 @@ export default function AddFamilyForm() {
               <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
             </button>
-            <button onClick={handleReturn} className="button manageSubmit mr-8 justify-between hover:bg-red-500">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <button className="button manageSubmit mr-8 justify-between hover:bg-red-500">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 1 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
               </svg>
             </button>
